42c4eb703c6b38f2898b69133a22aa1eb6ee8b5e
jbernal <miw@etsisi.upm.es>
2019-08-15 19:07:31 +0200

    add GET /Suggestions. #5

--- src/main/java/es/upm/miw/apaw_ep_themes/bussines_controllers/SuggestionBusinessController.java
+++ src/main/java/es/upm/miw/apaw_ep_themes/bussines_controllers/SuggestionBusinessController.java
@@ -6,6 +6,9 @@ import es.upm.miw.apaw_ep_themes.dtos.SuggestionDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class SuggestionBusinessController {
 
@@ -21,6 +24,11 @@ public class SuggestionBusinessController {
         Suggestion suggestion = new Suggestion(suggestionDto.getNegative(), suggestionDto.getDescription());
         this.suggestionDao.save(suggestion);
         return new SuggestionDto(suggestion);
+    }
+
+    public List<SuggestionDto> readAll() {
+        List<Suggestion> suggestions = this.suggestionDao.findAll();
+        return suggestions.stream().map(SuggestionDto::new).collect(Collectors.toList());
     }
 
 }
