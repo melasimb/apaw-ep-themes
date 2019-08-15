75c018b0a710d6486126fa953538cddbe092a787
jbernal <miw@etsisi.upm.es>
2019-08-15 15:21:10 +0200

    Initial. apaw-ep-themes. 2019-20

---
+++ src/main/java/es/upm/miw/apaw_ep_api/SystemResource.java
@@ -0,0 +1,28 @@
+package es.upm.miw.apaw_ep_api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(SystemResource.SYSTEM)
+public class SystemResource {
+
+    public static final String SYSTEM = "/system";
+    public static final String VERSION = "/version";
+
+    @Value("${application.name}")
+    private String applicationName;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Value("${build.timestamp}")
+    private String buildTimestamp;
+
+    @GetMapping(SystemResource.VERSION)
+    public VersionDto readVersion() { // http://localhost:8080/system/version
+        return new VersionDto(this.applicationName + "::" + this.buildVersion + "::" + this.buildTimestamp);
+    }
+}
---
+++ src/test/java/es/upm/miw/apaw_ep_api/SystemResourceIT.java
@@ -0,0 +1,25 @@
+package es.upm.miw.apaw_ep_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ApiTestConfig
+class SystemResourceIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testReadVersion() {
+        VersionDto versionDto = this.webTestClient
+                .get().uri(SystemResource.SYSTEM + SystemResource.VERSION)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(VersionDto.class)
+                .returnResult().getResponseBody();
+        assertNotNull(versionDto.getVersion());
+    }
+}
