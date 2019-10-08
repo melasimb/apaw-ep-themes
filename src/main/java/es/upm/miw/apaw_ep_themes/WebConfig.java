bb94dad141e44c92f20ff462ec3bbbd5dec620e0
jbernal <miw@etsisi.upm.es>
2019-10-08 13:24:11 +0200

    add cross domain

---
+++ src/main/java/es/upm/miw/apaw_ep_themes/WebConfig.java
@@ -0,0 +1,15 @@
+package es.upm.miw.apaw_ep_themes;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").maxAge(3600);
+    }
+
+}
